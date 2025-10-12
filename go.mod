93403b066d17036965171f2d1822d1d3beed1604
zlongxiang <1251885400@qq.com>
2025-10-12 12:52:40 +0800

    更新

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/before80/go_docs
 go 1.22.3
 
 require (
-	github.com/google/docsy v0.10.0 // indirect
+	github.com/google/docsy v0.12.0 // indirect
 	github.com/google/docsy/dependencies v0.7.2 // indirect
 )
--- static/js/deal-zero-width-space.js
+++ static/js/deal-zero-width-space.js
@@ -8,4 +8,32 @@
             node.style.textIndent = "2em"
         }
     }
+
+    function adjustTocHeight() {
+        const toc = document.querySelector('.td-sidebar-toc');
+        const mainContent = document.querySelector('main');
+        const tdToc = toc.querySelector(".td-toc")
+    
+        if (toc && mainContent) {
+            // const mainHeight = mainContent.scrollHeight;
+            const viewportHeight = window.innerHeight;
+            const tdTocHeight = tdToc.scrollHeight;
+            console.log("tdTocHeight=",tdTocHeight)
+            // 156为.td-toc元素上面一些元素的高度，12是padding-top的高度，24是padding-bottom的高度
+            let tocH = tdTocHeight + 156 + 12 + 24 + 1;
+
+            // 150为footer元素的高度,64是头部菜单的高度
+            if (tocH  < viewportHeight - 150 - 64 ) {
+                toc.style.maxHeight = tocH + 'px';
+                toc.style.overflowY = "hidden"
+            } else {
+                toc.style.maxHeight = tocH + 10 + 'px';
+                toc.style.overflowY = "auto"
+            }
+        }
+    }
+
+    // 在页面加载和调整大小时调用
+    window.addEventListener('load', adjustTocHeight);
+    window.addEventListener('resize', adjustTocHeight);
 })();
